4260a1ed94cd2d2f296888ec6dbe13c694083f97
zaruun <mucha.mateusz@hotmail.com>
2023-07-02 21:05:36 +0200

    define colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      decoration: "#FCA311",
+      primary: "#14213D",
+      seckondary: "#FCA311",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
