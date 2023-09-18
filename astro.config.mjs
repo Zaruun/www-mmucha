5c09a7d0a237519ed99eb1790546ce9628ed76a8
zaruun <mucha.mateusz@hotmail.com>
2023-09-18 20:49:40 +0200

    init v3 & tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
-import { defineConfig } from "astro/config";
-import relativeLinks from "astro-relative-links";
+import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mmucha.pl",
-  integrations: [relativeLinks(), tailwind()],
+  integrations: [tailwind()]
 });
